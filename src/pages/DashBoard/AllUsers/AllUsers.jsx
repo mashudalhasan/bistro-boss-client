a4d154a05e5c7efb129a0444407637f61771a137
mashudalhasan <mashudalhasan554@gmail.com>
2023-05-31 10:50:12 +0600

    jwt token functionality added

--- src/pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/pages/DashBoard/AllUsers/AllUsers.jsx
@@ -29,7 +29,30 @@ const AllUsers = () => {
       });
   };
 
-  const handleDelete = () => {};
+  const handleDelete = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/${user._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire("Deleted!", "User has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="w-full">
